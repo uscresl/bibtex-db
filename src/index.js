3aaf5c0464b6618f66802c2d80f29bf41a31075c
Vedant Mistry <vedantbmistry1309@gmail.com>
2019-10-31 15:16:41 -0700

    [update]: handled-cases where citation.js could not recognize tags

--- src/index.js
+++ src/index.js
@@ -2465,14 +2465,20 @@ function groupByYear(bibtexParseJSData, citationJSData) {
             let citation = {}
             if (citationJSData[index].title != undefined) {
                 citation[TITLE] = citationJSData[index].title;
+            } else {
+                citation[TITLE] = entrytags.title
             }
 
             if (citationJSData[index].author != undefined) {
                 citation[AUTHOR] = citationJSData[index].author;
+            } else {
+                citation[AUTHOR] = entrytags.author
             }
 
             if (citationJSData[index]["container-title"] != undefined) {
                 citation[BOOKTITLE] = citationJSData[index]["container-title"];
+            } else {
+                citation[BOOKTITLE] = entrytags.booktitle
             }
 
             if (entrytags.status != undefined) {
@@ -2489,14 +2495,20 @@ function groupByYear(bibtexParseJSData, citationJSData) {
             let citation = {}
             if (citationJSData[index].title != undefined) {
                 citation[TITLE] = citationJSData[index].title;
+            } else {
+                citation[TITLE] = entrytags.title
             }
 
             if (citationJSData[index].author != undefined) {
                 citation[AUTHOR] = citationJSData[index].author;
+            } else {
+                citation[AUTHOR] = entrytags.author
             }
 
             if (citationJSData[index]["container-title"] != undefined) {
                 citation[BOOKTITLE] = citationJSData[index]["container-title"];
+            } else {
+                citation[BOOKTITLE] = entrytags.booktitle
             }
 
             if (entrytags.year != undefined) {
@@ -2523,19 +2535,27 @@ function prepareBibTeXContentDiv(contentData) {
         contentData[year].forEach(element => {
             contentString += "<li>";
             if (element.author != undefined) {
-                element.author.forEach(author => {
-                    if (author.given != undefined) {
-                        contentString += author.given;
-                        if (author.family != undefined) {
-                            contentString += author.family;
+                if (Array.isArray(element.author)) {
+                    element.author.forEach((author, index) => {
+                        if (author.given != undefined) {
+                            if (element.author.length > 1 && (index == element.author.length - 1)) {
+                                contentString = contentString.substring(0, contentString.length - 2);
+                                contentString += " and ";
+                            }
+                            contentString += author.given + " ";
+                            if (author.family != undefined) {
+                                contentString += author.family;
+                            }
+                            contentString += ", ";
+                        } else {
+                            if (author.literal != undefined) {
+                                contentString += author.literal + ", ";
+                            }
                         }
-                        contentString += ", ";
-                    } else {
-                        if (author.literal != undefined) {
-                            contentString += author.literal + ", ";
-                        }
-                    }
-                });
+                    });
+                } else {
+                    contentString += element.author + ", "
+                }
             }
             if (element.title != undefined) {
                 contentString += element.title + ", ";
@@ -2561,11 +2581,14 @@ function handler() {
             let parsedBibTeX = bibTeXParse.toJSON(allText);
             parseAsync(allText).then((result) => {
                 let lowerCaseKeysParsedBibTeX = convertKeysToLowerCase(parsedBibTeX);
-                console.log(lowerCaseKeysParsedBibTeX);
+                // console.log(lowerCaseKeysParsedBibTeX);
+
                 let citationJSParsedData = cite.set(result).get(opt)
-                console.log(citationJSParsedData);
+                // console.log(citationJSParsedData);
+                
                 let bibTeXDataGroupedByYear = groupByYear(lowerCaseKeysParsedBibTeX, citationJSParsedData);
                 // console.log(bibTeXDataGroupedByYear);
+                
                 prepareBibTeXContentDiv(bibTeXDataGroupedByYear);
             });
             
