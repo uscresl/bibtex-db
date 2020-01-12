d3f3f5068d68cf29d2311d1e4989f10a2f21e9ed
Vedant Mistry <vedantbmistry1309@gmail.com>
2020-01-12 01:50:14 -0800

    [feat]: functionality to read from separate file with a given heading (in-progress)

--- src/index.js
+++ src/index.js
@@ -2730,6 +2730,63 @@ function filterPublications(familyNames, givenNames, combinedParsedData) {
 }
 
 /**
+* Takes in an Array of parsed publications and returns a grouping by type.
+* @param {Array} parsedPublications Array of parsed publications
+* @param {boolean} includeUnderReview Boolean which returns under-review status publications if true
+* @returns {Object} Object which includes publications grouped by type
+*/
+function groupPublicationsByType(parsedPublications, includeUnderReview) {
+    let publicationsGroupedByType = {};
+    for (let index = 0; index < parsedPublications.length; index++) {
+        let publication = parsedPublications[index];
+        let bibTeXParseElement = publication[BIBTEX_PARSE_JS];
+        let bibTeXParseEntryTags = bibTeXParseElement.entrytags;
+        
+        // Check if under-review or accepted papers are to be included.
+        if (bibTeXParseEntryTags.status != undefined) {
+            let status = bibTeXParseEntryTags.status;
+            if (!includeUnderReview && status == UNDER_REVIEW) {
+                continue;
+            }
+        }
+
+        // Creating a group of a particular entry type.
+        let entrytype = ""
+        if (bibTeXParseEntryTags.type == undefined) {
+            // Find the type of the publication.
+            if (bibTeXParseElement.entrytype == undefined) {
+                // default: CONFERENCE.
+                entrytype = CONFERENCE;
+            } else {
+                if (CATEGORY_MAPPING[bibTeXParseElement.entrytype.toLowerCase()] == undefined) {
+                    // default: CONFERENCE.
+                    entrytype = CONFERENCE;
+                } else {
+                    // assign the type if it's present in the CATEGORY_MAPPING.
+                    entrytype = CATEGORY_MAPPING[bibTeXParseElement.entrytype.toLowerCase()];
+                }
+            }
+        } else {
+            // if a field 'type' tag is provided, it overrides the entrytype.
+            if (CATEGORY_MAPPING[bibTeXParseEntryTags.type.toLowerCase()] == undefined) {
+                // default: CONFERENCE.
+                entrytype = CONFERENCE;
+            } else {
+                // assign the type if it's present in the CATEGORY_MAPPING.
+                entrytype = CATEGORY_MAPPING[bibTeXParseEntryTags.type.toLowerCase()];
+            }
+        }
+
+        if (publicationsGroupedByType[entrytype] == undefined) {
+            publicationsGroupedByType[entrytype] = [];
+        }
+
+        publicationsGroupedByType[entrytype].push(publication);
+    }
+    return publicationsGroupedByType;
+}
+
+/**
  * Takes in an Array of parsed publications and returns a grouping by year and type.
  * @param {Array} parsedPublications Array of parsed publications
  * @param {boolean} includeUnderReview Boolean which returns under-review status publications if true
@@ -2846,6 +2903,218 @@ function generateButtonLink(url, title) {
                     color: white; border: none; border-radius: 5px;">
                         ${title}
                     </a>`;
+}
+
+/**
+ * Displays the citations in a formatted manner inside the provided div class.
+ * @param {Array} publicationsGroupedByType Array of parsed (and filtered) publications
+ * @param {string} title Title to be given to the data
+ * @param {string} divClass Class of the div where the content is to be displayed
+ */
+function loadBibTeXContentDivByType(publicationsGroupedByType, title, divClass) {
+    let contentString = "";
+    // Year heading is appended.
+    contentString += `<div style="font-size: 20px; font-weight: bold;">
+                            ${title}
+                        </div>`;
+    for (let categoryIndex = 0; categoryIndex < CATEGORY_ORDERING.length; categoryIndex++) {
+        let category = CATEGORY_ORDERING[categoryIndex];
+        if (publicationsGroupedByType[category] != undefined) {
+            let categoryHeading = CATEGORY_HEADINGS[category];
+            // Category heading is appended.
+            contentString += `<div style="font-size: 15px; margin-left: 1em; margin-top: 10px; font-weight: bold;">
+                                    ${categoryHeading}
+                                </div>`;
+
+            contentString += `<ul>`;
+            let publicationsForCategory = publicationsGroupedByType[category];
+            for (let publicationIndex = 0; publicationIndex < publicationsForCategory.length; publicationIndex++) {
+                contentString += `<li>`;
+                
+                publication = publicationsForCategory[publicationIndex];
+                
+                let bibTeXParseJSElement = publication[BIBTEX_PARSE_JS];
+                let bibTeXParseEntryTags = bibTeXParseJSElement.entrytags;
+                let citationJSElement = publication[CITATION_JS];
+                
+                // Authors are handled below.
+                let citationJSAuthors = citationJSElement[AUTHOR];
+                let bibTeXParseJSAuthors = bibTeXParseEntryTags[AUTHOR];
+                if (citationJSAuthors != undefined) {
+                    for (let authorIndex = 0; authorIndex < citationJSAuthors.length; authorIndex++) {
+                        let author = citationJSAuthors[authorIndex];
+                        if (authorIndex == citationJSAuthors.length - 1 && citationJSAuthors.length > 1) {
+                            contentString = contentString.substring(0, contentString.length - 2);
+                            contentString += ` and `;
+                        }
+                        if (author.literal == undefined) {
+                            if (author.given != undefined && author.family != undefined) {
+                                if (author[NON_DROPPING_PARTICLE] != undefined) {
+                                    contentString += `${author.given} ${author[NON_DROPPING_PARTICLE]} 
+                                                        ${author.family}, `;
+                                } else {
+                                    contentString += `${author.given} ${author.family}, `;
+                                }
+                                
+                            } else if (author.given != undefined) {
+                                contentString += `${author.given}, `;
+                            } else if (author.family != undefined) {
+                                if (author[NON_DROPPING_PARTICLE] != undefined) {
+                                    contentString += `${author[NON_DROPPING_PARTICLE]} ${author.family}, `;
+                                } else {
+                                    contentString += `${author.family}, `;
+                                }
+                            }
+                        } else {
+                            contentString += `${author.literal}, `;
+                        }
+                    }
+                } else if (bibTeXParseJSAuthors != undefined) {
+                    contentString += `${bibTeXParseJSAuthors}, `;
+                }
+
+                // Editors are handled below.
+                let citationJSEditors = citationJSElement[EDITOR];
+                let bibTeXParseJSEditors = bibTeXParseEntryTags[EDITOR];
+                if (citationJSEditors != undefined) {
+                    for (let editorIndex = 0; editorIndex < citationJSEditors.length; editorIndex++) {
+                        let editor = citationJSEditors[editorIndex];
+                        if (editorIndex == citationJSEditors.length - 1 && citationJSEditors.length > 1) {
+                            contentString = contentString.substring(0, contentString.length - 2);
+                            contentString += ` and `;
+                        }
+                        if (editor.literal == undefined) {
+                            if (editor.given != undefined && editor.family != undefined) {
+                                if (editor[NON_DROPPING_PARTICLE] != undefined) {
+                                    contentString += `${editor.given} ${editor[NON_DROPPING_PARTICLE]} 
+                                                        ${editor.family}, `;
+                                } else {
+                                    contentString += `${editor.given} ${editor.family}, `;
+                                }
+                                
+                            } else if (editor.given != undefined) {
+                                contentString += `${editor.given}, `;
+                            } else if (editor.family != undefined) {
+                                if (editor[NON_DROPPING_PARTICLE] != undefined) {
+                                    contentString += `${editor[NON_DROPPING_PARTICLE]} ${editor.family}, `;
+                                } else {
+                                    contentString += `${editor.family}, `;
+                                }
+                            }
+                        } else {
+                            contentString += `${editor.literal}, `;
+                        }
+                    }
+                } else if (bibTeXParseJSEditors != undefined) {
+                    contentString += `${bibTeXParseJSEditors}, `;
+                }
+
+                // Title is handled below.
+                if (citationJSElement[TITLE] != undefined) {
+                    contentString += `${citationJSElement[TITLE]}, `;
+                } else if (bibTeXParseEntryTags[TITLE] != undefined) {
+                    contentString += `${bibTeXParseEntryTags[TITLE]}, `;
+                }
+
+                // Conference/journal name is handled below.
+                if (citationJSElement[CONTAINER_TITLE] != undefined) {
+                    if (bibTeXParseEntryTags[STATUS] != undefined && bibTeXParseEntryTags[STATUS] == UNDER_REVIEW) {
+                        contentString += `Under review at `;
+                    } else {
+                        contentString += `In `;
+                    }
+                    contentString += `${citationJSElement[CONTAINER_TITLE]}, `;
+                } else if (bibTeXParseEntryTags[JOURNAL] != undefined) {
+                    if (bibTeXParseEntryTags[STATUS] != undefined && bibTeXParseEntryTags[STATUS] == UNDER_REVIEW) {
+                        contentString += `Under review at `;
+                    } else {
+                        contentString += `In `;
+                    }
+                    contentString += `${bibTeXParseEntryTags[JOURNAL]}, `;
+                }
+
+                // Year is handled here.
+                if (bibTeXParseEntryTags[YEAR] != undefined) {
+                    contentString += `${bibTeXParseEntryTags[YEAR]}, `;
+                }
+
+                // Terminating the content with a full stop.
+                contentString = contentString.substring(0, contentString.length - 2);
+                contentString += `.`;
+
+                /* Url buttons are handled here. This includes the following:
+                    - url_site
+                    - url_preprint
+                    - url_code
+                    - url_dataset
+                    - url_poster
+                    - url_slides
+                    - url_video
+                */
+                contentString += `<br/>`
+                if (bibTeXParseEntryTags[URL_SITE] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_SITE], BUTTON_TITLES[URL_SITE]);
+                }
+
+                if (bibTeXParseEntryTags[URL_PREPRINT] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_PREPRINT], BUTTON_TITLES[URL_PREPRINT]);
+                }
+
+                if (bibTeXParseEntryTags[URL_CODE] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_CODE], BUTTON_TITLES[URL_CODE]);
+                }
+
+                if (bibTeXParseEntryTags[URL_DATASET] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_DATASET], BUTTON_TITLES[URL_DATASET]);
+                }
+
+                if (bibTeXParseEntryTags[URL_POSTER] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_POSTER], BUTTON_TITLES[URL_POSTER]);
+                }
+
+                if (bibTeXParseEntryTags[URL_SLIDES] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_SLIDES], BUTTON_TITLES[URL_SLIDES]);
+                }
+
+                if (bibTeXParseEntryTags[URL_VIDEO] != undefined) {
+                    contentString += generateButtonLink(bibTeXParseEntryTags[URL_VIDEO], BUTTON_TITLES[URL_VIDEO]);
+                }
+
+                // Show/hide citation button is attached.
+                let citationKey = bibTeXParseJSElement.citationkey
+                                    + Math.random().toString(36).substring(2,15);
+                contentString += `<button id="button-${citationKey}"
+                                        class="button-${citationKey}"
+                                        style="background-color: #880000; margin-left: 1vw; font-size: 13.33px;
+                                                        padding-left: 15px; padding-right: 15px;
+                                                        color: white; border: none; border-radius: 5px;">
+                                        Show Citation
+                                    </button>`;
+                contentString += `<div id="citation-${citationKey}"
+                                    class="citation-${citationKey}"
+                                    style="background-color: #e8e8e8; padding: 5px; margin: 5px; color: black;
+                                                border-radius: 5px; font-size: 11px; display: none;
+                                                font-family: 'Courier New', Courier, monospace;">
+                                        ${getCitationContent(bibTeXParseJSElement)}
+                                    </div>`;
+                contentString += ""
+                contentString += `<script>
+                                        $(".button-${citationKey}").click(() => {
+                                            $(".citation-${citationKey}").toggle();
+                                            if ($(".citation-${citationKey}").is(":visible")) {
+                                                $(".button-${citationKey}").html("Hide Citation");
+                                            } else {
+                                                $(".button-${citationKey}").html("Show Citation");
+                                            }
+                                        });
+                                    </script>`
+                contentString += `</li>`;
+            }
+            contentString += `</ul>`
+        }
+    }
+    // Append content to the div.
+    $("." + divClass).append(contentString);
 }
 
 /**
@@ -3119,6 +3388,7 @@ function readAndLoadBibtexDBFor(familyNames, givenNames, filename, divClass) {
  * @param {string} divClass Class of the div where the content is to be displayed 
  */
 function readAndLoadAllBibtexDB(filename, divClass) {
+    document.write(`<div id="bibtex-content" class="bibtex-content"></div>`)
     fetch(filename).then((response) => {
         response.text().then((allText) => {
             try {
@@ -3145,9 +3415,41 @@ function readAndLoadAllBibtexDB(filename, divClass) {
     });
 }
 
+function readAndLoadBibtexFrom(filename, title) {
+    let divClass = `bibtex-${title}-` + Math.random().toString(36).substring(2,15);
+    document.write(`<div id="${divClass}" class="${divClass}"></div>`)
+    fetch(filename).then((response) => {
+        response.text().then((allText) => {
+            try {
+                // parse the file text.
+                let parsedBibTeX = bibTeXParse.toJSON(allText);
+                
+                // convert all keys to lowercase to enable ease of access.
+                let lowerCaseKeysParsedBibTeX = convertKeysToLowerCase(parsedBibTeX);
+                
+                // get combined data which includes bibtex-parse-js and citation-js data.
+                let combinedParsedData = getCombinedParsedData(lowerCaseKeysParsedBibTeX);
+                console.log(combinedParsedData);
+
+                // group the data by year and type.
+                let bibTeXDataGroupedByType = groupPublicationsByType(combinedParsedData, true);
+                console.log(bibTeXDataGroupedByType)
+
+                // load the parsed and grouped content into a div on the page.
+                // loadBibTeXContentDivFromData(bibTeXDataGroupedByYearAndType, divClass);
+                loadBibTeXContentDivByType(bibTeXDataGroupedByType, title, divClass);
+            } catch (error) {
+                console.log(error);
+                $(divClass).append("BibTeX file parsing or internal error.");
+            }
+        });
+    });
+}
+
 // Globalizing the variables for access outside the bundled script.
 window.getPublicationsFor = readAndLoadBibtexDBFor;
 window.getAllPublications = readAndLoadAllBibtexDB;
+window.getAllPublicationsFromFile = readAndLoadBibtexFrom;
 },{"bibtex-parse-js":110,"citation-js":112,"remove-accents":117}],9:[function(require,module,exports){
 "use strict";
 
